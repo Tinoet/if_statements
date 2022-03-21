244a7c03f835c476611617a86af5f9e78e9f017c
Tinoet <88379020+Tinoet@users.noreply.github.com>
2022-03-21 23:23:55 +0100

    Add files via upload

---
+++ If_statements/if.py
@@ -0,0 +1,25 @@
+# from operator import truediv
+
+
+# is_hot = False
+# is_cold = False
+# if is_hot :
+#    print("it's a hot") 
+#    print("drink plenty of water")
+# elif is_cold:
+#   print("its a cold day")
+#   print("wear a warm cloth")
+# else:
+#   print("have a lovely day")
+
+
+price = 1000000
+has_good_credit =False
+ 
+if has_good_credit: 
+   down_payment = 0.1 * price
+
+else:
+  down_payment = 0.2 * price
+
+print(f"downpayment: ${down_payment}")
